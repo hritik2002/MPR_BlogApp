8cf5b0d299c31b7d9d5959bff4875071f3fcdcf7
hritik2002 <sharmahritik2002@gmail.com>
2021-10-13 10:41:14 +0530

    Add Footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Footer.css'
+
+const Footer= ()=>{
+    return (
+        <div className="Footer_container">
+            <div className="Footer_content">
+                <div className="first">
+                    <a href="#">About us</a>
+                    <a href="#">Team</a>
+                    <a href="#">Contact us</a>
+                </div>
+                <div className="second">
+                    <a href="#">Aneesh</a>
+                    <a href="#">Bhoomi</a>
+                    <a href="#">Hritik</a>
+                </div>
+                <div className="third">
+                    <div class="h4">Follow us</div>
+                    <a href="#">Instagram</a>
+                    <a href="#">Linkedn</a>
+                    <a href="#">Twitter</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer/Footer';
 
 const Home = ()=>{
     return (
         <div class="Home_container" >
             <Navbar />
+            <Footer />
         </div>
     )
 }
